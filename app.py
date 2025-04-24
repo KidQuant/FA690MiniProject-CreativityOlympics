2db94dbf21ffe0f5ce820b5dac304dd24332ccc8
KidQuant <38800128+KidQuant@users.noreply.github.com>
2025-04-24 01:12:42 -0400

    working resume analyzer on app.py

--- app.py
+++ app.py
@@ -2,20 +2,74 @@ import streamlit as st
 from resume_parser import parse_resume
 import random
 from langchain_utils import extract_keywords, generate_resume_content, update_resume
+from langchain_analyze import summary_prompt
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.question_answering import load_qa_chain
+from langchain.vectorstores import FAISS
+import openai
 from scrape import scrape_resume, fetch_url_content
+from PyPDF2 import PdfReader
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+openai.api_key = os.environ['OPENAI_API_KEY']
 
 st.title("Resume Analyzer and Optimizer")
-action = st.radio("Select Action:", ("Analyze Resume", 
-                                     "Create a New Resume", 
-                                     "Update Existing Resume"))
+action = st.radio(
+    "Select Action:",
+    ("Analyze Resume", "Create a New Resume", "Update Existing Resume"),
+)
 
-# Initialize session state for visibility
-if 'show_inputs' not in st.session_state:
-    st.session_state.show_inputs = True
 
 # Function to toggle input visibility
 def toggle_inputs():
     st.session_state.show_inputs = not st.session_state.show_inputs
+
+
+def pdf_to_chunks(pdf):
+    # read pdf and it returns memory addresss
+    pdf_reader = PdfReader(pdf)
+
+    # extract text forom each page separately
+    text = ""
+    for page in pdf_reader.pages:
+        text += page.extract_text()
+
+    # Split the long text into small chunks
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=700, chunk_overlap=200, length_function=len
+    )
+
+    chunks = text_splitter.split_text(text=text)
+    return chunks
+
+
+def openai_function(openai_api_key, chunks, analyze):
+
+    # Using OpenAI service for embedding
+    embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
+
+    # Facebook AI Similiarity Search (FAISS) to help us convert text data to numerical vector
+    vectorstores = FAISS.from_texts(chunks, embedding=embeddings)
+
+    # compares the query and chunks, enabling the selection of the top 'K' most similiar chunks based on their similiarity score
+    docs = vectorstores.similarity_search(query=analyze, k=3)
+
+    # creates an OpenAI object, using the ChatGPT 4o model
+    llm = ChatOpenAI(model="gpt-4o", api_key=openai_api_key)
+
+    # question-answering (QA) pipeline, making use of the load_qa_chain function
+    chain = load_qa_chain(llm=llm, chain_type="stuff")
+
+    response = chain.run(input_documents=docs, question=analyze)
+    return response
+
+
+# Initialize session state for visibility
+if "show_inputs" not in st.session_state:
+    st.session_state.show_inputs = True
 
 # Display file uploader for specific actions
 if action in ["Analyze Resume", "Update Existing Resume"]:
@@ -31,24 +85,35 @@ if action != "Analyze Resume":
 if st.button("Process"):
     if action == "Analyze Resume" and uploaded_file is not None:
         toggle_inputs()  # Hide inputs when processing starts
-        # Add your resume analysis logic here
 
-    if action == "Update Existing Resume" and uploaded_file is not None and job_description and role:
+        pdf_chunks = pdf_to_chunks(uploaded_file)
+        analyze_prompt = summary_prompt(query_with_chunks=pdf_chunks)
+        response = openai_function(openai_api_key=openai.api_key, chunks=pdf_chunks, analyze=analyze_prompt)
+        st.write(response)
+
+    if (
+        action == "Update Existing Resume"
+        and uploaded_file is not None
+        and job_description
+        and role
+    ):
         resume_text = parse_resume(uploaded_file)
         keywords = extract_keywords(job_description)
         urls_keywords = scrape_resume(role)
 
         fetched_data = [fetch_url_content(url) for url in urls_keywords]
 
-        updated_resume = update_resume(original_resume=resume_text,
-                                       role=role, 
-                                       keywords=keywords,
-                                       fetched_resumes=fetched_data)
-        
-        st.download_button("Download Updated Resume",
-                           updated_resume,
-                           file_name="updated_resume.txt")
-        
+        updated_resume = update_resume(
+            original_resume=resume_text,
+            role=role,
+            keywords=keywords,
+            fetched_resumes=fetched_data,
+        )
+
+        st.download_button(
+            "Download Updated Resume", updated_resume, file_name="updated_resume.txt"
+        )
+
         st.write("Updated Resume:")
         st.write(updated_resume)
 
@@ -61,14 +126,16 @@ if st.button("Process"):
 
         fetched_data = [fetch_url_content(url) for url in urls_keywords]
 
-        new_resume_content = generate_resume_content(keywords=keywords,
-                                                     role=role,
-                                                     fetched_resumes=fetched_data)
-        
-        st.download_button("Download New Generated Resume",
-                           new_resume_content,
-                           file_name="new_resume.txt")
-        
+        new_resume_content = generate_resume_content(
+            keywords=keywords, role=role, fetched_resumes=fetched_data
+        )
+
+        st.download_button(
+            "Download New Generated Resume",
+            new_resume_content,
+            file_name="new_resume.txt",
+        )
+
         st.write("Generated Resume:")
         st.write(new_resume_content)
 
--- langchain_analyze.py
+++ langchain_analyze.py
@@ -1,9 +1,3 @@
-from PyPDF2 import PdfReader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.chat_models import ChatOpenAI
-from langchain.chains.question_answering import load_qa_chain
 import warnings
 
 warnings.filterwarnings("ignore")
@@ -14,51 +8,13 @@ from dotenv import load_dotenv
 load_dotenv()
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
-
-def pdf_to_chunks(pdf):
-    # read pdf and it returns memory addresss
-    pdf_reader = PdfReader(pdf)
-
-    # extract text forom each page separately
-    text = ""
-    for page in pdf_reader.pages:
-        text += page.extract_text()
-
-    # Split the long text into small chunks
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=700, chunk_overlap=200, length_function=len
-    )
-
-    chunks = text_splitter.split_text(text=text)
-
-
-def openai_function(openai_api_key, chunks, analyze):
-
-    # Using OpenAI service for embedding
-    embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
-
-    # Facebook AI Similiarity Search (FAISS) to help us convert text data to numerical vector
-    vectorstores = FAISS.from_texts(chunks, embedding=embeddings)
-
-    # compares the query and chunks, enabling the selection of the top 'K' most similiar chunks based on their similiarity score
-    docs = vectorstores.similarity_search(query=analyze, k=3)
-
-    # creates an OpenAI object, using the ChatGPT 4o model
-    llm = ChatOpenAI(model="gpt-4o", api_key=openai_api_key)
-
-    # question-answering (QA) pipeline, making use of the load_qa_chain function
-    chain = load_qa_chain(llm=llm, chain_type="stuff")
-
-    response = chain.run(input_documents=docs, question=analyze)
-    return response
-
-
 def summary_prompt(query_with_chunks):
     query = f'''
     I need a detailed summary of the resume below. Finally, provide us with a conclusion
     """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
     {query_with_chunks}
-
     """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
     '''
     return query
+
+
