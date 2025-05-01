b04656667b84d033da8043da631d1c9ecfb831a0
KidQuant <38800128+KidQuant@users.noreply.github.com>
2025-05-01 17:27:43 -0400

    merging langchain files

--- langchain_prompts.py
+++ langchain_prompts.py
@@ -36,6 +36,7 @@ def weaknesses_prompt(query_with_chunks):
     '''
     return query
 
+
 def job_title_prompt(query_with_chunks):
     query = f'''Based on my resume, come up with some job roles that would best fit my skills and abilities.
                 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
@@ -44,12 +45,27 @@ def job_title_prompt(query_with_chunks):
                 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
                 '''
     return query
-            
+
+
 def analyze_skills_from_jobs(jobs_json):
-    query = f'''Analyze the following job data to extract the most important skills for candidates:
+    query = f"""Analyze the following job data to extract the most important skills for candidates:
                 \"\"\"{jobs_json}\"\"\"
-             '''
+             """
     return query
 
 
+# Added query for cover letter option
+def cover_letter_prompt(resume_text, job_description, role):
+    query = f'''You are a professional cover letter writer. Based on my resume and the job description below, generate a formal and customized cover letter for the position of {role}. The tone should be confident, professional, and highlight relevant skills and motivation.
 
+                Resume:
+                """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+                {resume_text}
+                """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+                Job Description:
+                """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+                {job_description}
+                """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+                '''
+    return query
